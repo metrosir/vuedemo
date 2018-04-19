7681bef72aa7cb7f16d5d0b0b9c1cffe820ecfe1
zhengqilin <zhengqilin@mcmc.com>
2018-04-19 18:55:14 +0800

    vue demo

---
+++ t6/app.js
@@ -0,0 +1,19 @@
+
+new Vue({
+	el: "#vue-app",
+	data: {
+		error: false,
+		success: false
+	},
+	methods: {
+
+	},
+	//计算属性
+	computed: {
+	}
+});
+
+// v-if 条件判断
+// v-else-if
+
+// v-show 占位隐藏、显示
